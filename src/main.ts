919516a02ef7e5134ef04e7049e2254b781e3d6e
Todd <todd.griffin61@outlook.com>
2024-03-13 10:37:02 +0000

    added search

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import "./styles/style.scss";
 import pokemonArray from "./data/pokemon"
 
+type State = {
+  searchTerm: string
+}
+let state: State = {
+  searchTerm: ""
+}
+
 const toProperCase = (string: string) => {
   return string.replace(/\w\S*/g, (txt) => {
     return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
@@ -12,24 +19,32 @@ const filterMatchesName = (searchItem: string, userSearch: string) => {
     return true
   } else if (searchItem.toLowerCase().includes(userSearch.toLowerCase())) {
     return true
-  } 
+  }
   return false
 }
 
 const pokeHeading = document.querySelector("h1")
-if (!pokeHeading) throw	new Error("Heading not found")
+if (!pokeHeading) throw new Error("Heading not found")
 const input = document.createElement("input")
 input.style.display = "block"
 input.placeholder = "PokéSearch"
 pokeHeading.appendChild(input)
+input.addEventListener("input", (event: Event) => {
+	const target = event.target as HTMLInputElement
+	state = { ...state, searchTerm: target.value }
+	displayCards()
+})
 
 const cardContainer = document.querySelector<HTMLDivElement>(".card-container")
 if (!cardContainer) throw new Error("Card container not found")
 
-pokemonArray.forEach(pokemon => {
-  const properName = toProperCase(pokemon.name);
+const displayCards = () => {
+  cardContainer.innerHTML = ""
+  pokemonArray.forEach(pokemon => {
+    if (!filterMatchesName(pokemon.name, state.searchTerm)) return
+    const properName = toProperCase(pokemon.name);
 
-  cardContainer.innerHTML += `
+    cardContainer.innerHTML += `
 	<section class="card">
 		<img class="card__image" src="${pokemon.sprite}" alt="">
 			<div class="card__content">
@@ -38,5 +53,7 @@ pokemonArray.forEach(pokemon => {
 		</div>
 	</section>
 	`
-})
+  })
+}
 
+displayCards()
