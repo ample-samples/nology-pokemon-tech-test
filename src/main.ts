872750907ed33d73a95fb6919b087a48d1d3eefd
Todd <todd.griffin61@outlook.com>
2024-03-13 10:12:11 +0000

    completed basic requirements

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,26 @@
 import "./styles/style.scss";
+import pokemonArray from "./data/pokemon"
+
+const toProperCase = (string: string) => {
+  return string.replace(/\w\S*/g, (txt) => {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
+};
+
+const cardContainer = document.querySelector<HTMLDivElement>(".card-container")
+if (!cardContainer) throw new Error("Card container not found")
+
+pokemonArray.forEach(pokemon => {
+  const properName = toProperCase(pokemon.name);
+
+  cardContainer.innerHTML += `
+	<section class="card">
+		<img class="card__image" src="${pokemon.sprite}" alt="">
+			<div class="card__content">
+			<h2 class="card__heading">${properName}</h2>
+			<p class="card__text">${properName} (#${pokemon.id}) is a ${pokemon.types.join(" & ")} type pokemon.</p>
+		</div>
+	</section>
+	`
+})
+
